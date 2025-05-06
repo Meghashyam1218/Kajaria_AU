62ad2925a05ed5f7bf37a8ff8c4064b87f04cff9
Meghashyam1218 <gurrammeghashyam@outlook.com>
2025-05-06 19:16:13 +0530

    responsive products page

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -6,7 +6,9 @@
 	console.log(extractedData);
 </script>
 
-<div class="products-grid mx-auto grid max-w-[100rem] grid-cols-4 items-center gap-4 p-6">
+<div
+	class="products-grid mx-auto grid max-w-[100rem] items-center gap-4 p-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
+>
 	{#each extractedData as product}
 		<Product {product} />
 	{/each}
